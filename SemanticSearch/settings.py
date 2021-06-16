62bed2461ff69be06e779666286bd93486f3420b
Khang Le <tkhang1999@gmail.com>
2021-06-16 20:31:02 +0800

    add static root

--- SemanticSearch/settings.py
+++ SemanticSearch/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
