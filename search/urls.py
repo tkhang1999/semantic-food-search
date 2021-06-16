98680e78758f671b06b013ef40bae372077343a7
Khang Le <tkhang1999@gmail.com>
2021-06-16 19:51:37 +0800

    add search form

--- search/urls.py
+++ search/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
 ]
--- search/views.py
+++ search/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+
+def search(request):
+    query = request.GET.get('q')
+    results = [
+        "Good food",
+        "Awesome food"
+    ]
+
+    args = {
+        'q': query,
+        'results': results,
+    }
+
+    return render(request, 'search.html', args)
