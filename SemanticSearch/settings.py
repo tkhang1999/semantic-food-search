031397f5e2b16650c403fa77563dadd6b907e065
Khang Le <tkhang1999@gmail.com>
2021-06-16 20:43:44 +0800

    add whitenoise to serve statics

--- SemanticSearch/settings.py
+++ SemanticSearch/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- SemanticSearch/wsgi.py
+++ SemanticSearch/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SemanticSearch.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
