4ffe7ec255782dd29f32e94bd4d9451287063979
Khang Le <tkhang1999@gmail.com>
2021-06-16 20:48:00 +0800

    fix whitenoise settings

--- SemanticSearch/settings.py
+++ SemanticSearch/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- SemanticSearch/wsgi.py
+++ SemanticSearch/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SemanticSearch.settings')
