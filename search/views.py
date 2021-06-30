29608de05a10bc6302a92c524288b7e6b8cb7fbc
Khang Le <tkhang1999@gmail.com>
2021-06-30 23:17:09 +0800

    handle error when solr has not finished loading

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.apps import apps
+from pysolr import SolrError
 
 # Create your views here.
 def home(request):
@@ -10,17 +11,23 @@ def search(request):
     query = request.GET.get('q') or ""
     top_results = request.GET.get('top') or "15"
     search_method = request.GET.get('method') or "bert"
+    solr_error = ""
 
     results = None
 
     if query:
-        results = apps.get_app_config('search').search_utils.search_reviews(query, top_results, search_method)
+        try:
+            results = apps.get_app_config('search').search_utils.search_reviews(query, top_results, search_method)
+        except SolrError:
+            solr_error = "Solr server may be loading at the moment, please wait for a while and try again!"
+
 
     args = {
         'q': query,
         'top': top_results,
         'method': search_method,
         'results': results,
+        'error': solr_error,
     }
 
     return render(request, 'search.html', args)
