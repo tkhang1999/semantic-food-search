6fd2d15acfaa8c9e2521f7a6bb8c2a02270302b7
Khang Le <tkhang1999@gmail.com>
2022-01-01 12:50:02 -0800

    prevent same form submission

--- search/urls.py
+++ search/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
-    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
 ]
--- search/views.py
+++ search/views.py
@@ -33,5 +33,5 @@ def search(request):
     return render(request, 'search.html', args)
 
 
-def contact(request):
-    return render(request, 'contact.html', {})
+def about(request):
+    return render(request, 'about.html', {})
