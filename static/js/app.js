f321e219acba68de447ddfe26d9565ea02dba625
Khang Le <tkhang1999@gmail.com>
2022-03-24 15:36:48 +0800

    add confirm modal for same form submission

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,47 @@
+/* Initialize Materialize components */
+
 document.addEventListener('DOMContentLoaded', function() {
     const elems = document.querySelectorAll('select');
-    const instances = M.FormSelect.init(elems);
+    M.FormSelect.init(elems);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
     const elems = document.querySelectorAll('.dropdown-trigger');
-    const instances = M.Dropdown.init(elems);
+    M.Dropdown.init(elems);
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const elems = document.querySelectorAll('.modal');
+    M.Modal.init(elems);
+});
+
+/* Helper functions */
+
 function showLoader() {
-    document.getElementById("loader").style.display = "block";
+    document.getElementById('loader').style.display = 'block';
 }
 
-function submitForm(e) {
+function submitForm() {
+    const form = document.querySelector('#search-form');
+
+    showLoader();
+    form.submit();
+}
+
+function processForm(e) {
     e.preventDefault();
 
     const urlParams = new URLSearchParams(window.location.search);
-    const form = document.querySelector('form');
-    const formData = new URLSearchParams(new FormData(form).entries());
+    const formData = new FormData(e.target);
 
-    if (urlParams.get("q") !== formData.get("q")
-        || urlParams.get("top") !== formData.get("top")
-        || urlParams.get("method") !== formData.get("method")) {
+    if (urlParams.get('q') == formData.get('q')
+        && urlParams.get('top') == formData.get('top')
+        && urlParams.get('method') == formData.get('method')) {
+        const elem = document.querySelector('#confirm-modal');
+        const instance = M.Modal.getInstance(elem);
+        instance.open();
+    } else {
         showLoader();
-        form.submit();
+        e.target.submit();
     }
 }
